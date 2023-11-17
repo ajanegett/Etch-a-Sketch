4d1031a6fb979bb811f1f98e930b43bb7f8dbceb
ajanegett <124137364+ajanegett@users.noreply.github.com>
2023-11-17 11:43:38 +0300

    Add grid-line toggler

--- javascript.js
+++ javascript.js
@@ -2,9 +2,9 @@ let slider = document.getElementById("myRange");
 let sliderOutput = document.querySelector(".gridVal");
 let gridDiv = document.getElementById("Grid");
 let borderChecker = document.querySelector("#gridLines");
-// let radio = document.querySelector('input[name="same"]:checked').value;
 
 sliderOutput.innerHTML = `${slider.value} x ${slider.value}`; // Display the default slider value
+createGrid(Number(slider.value));
 
 slider.oninput = function () {
   sliderOutput.innerHTML = `${this.value} x ${this.value}`;
@@ -12,10 +12,13 @@ slider.oninput = function () {
   createGrid(Number(slider.value));
 };
 
-borderChecker.addEventListener("onclick", function() {
-    console.log(borderChecker.checked);
-    return borderChecker.checked
-})
+borderChecker.oninput = function () {
+  console.log(this.checked);
+  let divList = document.querySelectorAll(".gridElement");
+  divList.forEach((node) => {
+    node.classList.toggle("noBorder");
+  });
+};
 
 /*
 Figure out getting input value from radio buttons and slider
@@ -26,22 +29,24 @@ Color methods
 */
 
 function getNumericStyle(val) {
-    let array = val.split("")
-    let newArray = array.filter((x) => {
-        if (Number(x) || Number(x) === 0) {
-            return true
-        }
-        return false
-    })
-    return newArray.join("")
+  let array = val.split("");
+  let newArray = array.filter((x) => {
+    if (Number(x) || Number(x) === 0) {
+      return true;
+    }
+    return false;
+  });
+  return newArray.join("");
 }
 
 function createGrid(SliderVal) {
   let i = 0;
-  while (i < (SliderVal * SliderVal)) {
+  while (i < SliderVal * SliderVal) {
     let div = document.createElement("div");
     div.classList.add("gridElement");
-    div.style = `width:${Number(getNumericStyle(gridDiv.style.width))/SliderVal}px; height:${Number(getNumericStyle(gridDiv.style.height))/SliderVal}px`;
+    div.style = `width:${
+      Number(getNumericStyle(gridDiv.style.width)) / SliderVal
+    }px; height:${Number(getNumericStyle(gridDiv.style.height)) / SliderVal}px`;
     gridDiv.appendChild(div);
     i++;
   }
