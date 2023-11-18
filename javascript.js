d3fb3e151e06308eb3e4add503f0233187d387a3
ajanegett <124137364+ajanegett@users.noreply.github.com>
2023-11-18 20:49:49 +0300

    Add lighter

--- javascript.js
+++ javascript.js
@@ -5,24 +5,24 @@ let borderChecker = document.querySelector("#gridLines");
 let allGridElements = document.querySelectorAll(".gridElement");
 let body = document.querySelector("body");
 let html = document.querySelector("html");
-let colorGetter = document.querySelector("#colorGrab")
-let bgColorGetter = document.querySelector("#BgGrab")
-let clearButton = document.querySelector("#clear")
-let penButton = document.querySelector("#pen")
-let rainbowButton = document.querySelector("#rainbowMode")
-let eraser = document.querySelector("#eraser")
-let shader = document.querySelector("#shader")
-let lighter = document.querySelector("#lighten")
-const buttonsArray = [penButton, rainbowButton, eraser, shader, lighter]
+let colorGetter = document.querySelector("#colorGrab");
+let bgColorGetter = document.querySelector("#BgGrab");
+let clearButton = document.querySelector("#clear");
+let penButton = document.querySelector("#pen");
+let rainbowButton = document.querySelector("#rainbowMode");
+let eraser = document.querySelector("#eraser");
+let shader = document.querySelector("#shader");
+let lighter = document.querySelector("#lighten");
+const buttonsArray = [penButton, rainbowButton, eraser, shader, lighter];
 
-let penmode = "pen"
+let penmode = "pen";
 
-buttonsArray.forEach(function(button) {
+buttonsArray.forEach(function (button) {
   button.oninput = function () {
     penmode = button.value;
     console.log(penmode);
-  } 
-})
+  };
+});
 
 sliderOutput.innerHTML = `${slider.value} x ${slider.value}`; // Display the default slider value
 createGrid(Number(slider.value));
@@ -41,14 +41,14 @@ borderChecker.oninput = function () {
   gridDiv.classList.toggle("noBorder");
 };
 
-bgColorGetter.oninput = function() {
-  setGridBgcolor()
-}
+bgColorGetter.oninput = function () {
+  setGridBgcolor();
+};
 
-clearButton.onclick = function() {
+clearButton.onclick = function () {
   gridDiv.innerHTML = "";
   createGrid(Number(slider.value));
-}
+};
 
 /*
 Figure out getting input value from radio buttons and slider
@@ -83,7 +83,8 @@ function createGrid(SliderVal) {
   allGridElements = document.querySelectorAll(".gridElement");
 }
 
-allGridElements.forEach(function (div) { // Add reactive divs
+allGridElements.forEach(function (div) {
+  // Add reactive divs
   div.addEventListener("mousedown", function mdown(e) {
     setGridElementColor(e);
     allGridElements.forEach(function (minidiv) {
@@ -111,49 +112,68 @@ body.addEventListener("mouseleave", function (e) {
     picodiv.removeEventListener("mouseover", setGridElementColor);
   });
 });
-html.addEventListener("contextmenu", function() {
-  allGridElements.forEach(function(element) {
-    element.removeEventListener("mouseover", setGridElementColor)
-  })
-})
+html.addEventListener("contextmenu", function () {
+  allGridElements.forEach(function (element) {
+    element.removeEventListener("mouseover", setGridElementColor);
+  });
+});
 
 // Set colors
 
 function setGridElementColor(event) {
-
   if (penmode === "pen") {
     event.target.style.backgroundColor = colorGetter.value;
-  }
-  else if (penmode === "eraser") {
+  } else if (penmode === "eraser") {
     event.target.style.backgroundColor = bgColorGetter.value;
-  }
-  else if (penmode === "rainbow") {
+  } else if (penmode === "rainbow") {
     event.target.style.backgroundColor = getRandomColor();
-  }
-  else if (penmode === "shader") {
-    let rgbVal = event.target.style.backgroundColor
+  } else if (penmode === "shader") {
+    let rgbVal = event.target.style.backgroundColor;
     if (rgbVal == "") {
-      event.target.style.backgroundColor = "rgb(240, 240, 240)"
+      event.target.style.backgroundColor = "rgb(240, 240, 240)";
     } else {
-      const arrayRgb = rgbVal.split("")
-      const myArray = arrayClear(arrayRgb)
-      let R = myArray[0]
-      let G = myArray[1]
-      let B = myArray[2]
-      if (R <20) {
-        R = 20
+      const arrayRgb = rgbVal.split("");
+      const myArray = arrayClear(arrayRgb);
+      let R = myArray[0];
+      let G = myArray[1];
+      let B = myArray[2];
+      if (R < 20) {
+        R = 20;
       }
       if (G < 20) {
-        G = 20
+        G = 20;
       }
       if (B < 20) {
-        B = 20
+        B = 20;
       }
-      event.target.style.backgroundColor = `rgb(${R-20}, ${G-20}, ${B-20})`
+      event.target.style.backgroundColor = `rgb(${R - 20}, ${G - 20}, ${
+        B - 20
+      })`;
+      console.log(event.target.style.backgroundColor);
+    }
+  } else {
+    let rgbVal = event.target.style.backgroundColor;
+    if (rgbVal == "") {
+      event.target.style.backgroundColor = "rgb(255, 255, 255)";
+    } else {
+      const arrayRgb = rgbVal.split("");
+      const myArray = arrayClear(arrayRgb);
+      let R = myArray[0];
+      let G = myArray[1];
+      let B = myArray[2];
+      if (R > 235) {
+        R = 235;
+      }
+      if (G > 235) {
+        G = 235;
+      }
+      if (B > 235) {
+        B = 235;
+      }
+      event.target.style.backgroundColor = `rgb(${R + 20}, ${G + 20}, ${B + 20})`;
       console.log(event.target.style.backgroundColor);
     }
   }
-
 }
 
 function setGridBgcolor() {
@@ -161,55 +181,54 @@ function setGridBgcolor() {
 }
 
 function getRandomColor() {
-  const rainbowColors = ["#ffbe0b", "#fb5607", "#ff006e", "#8338ec", "#3a86ff"]
-  const randomColor = rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
+  const rainbowColors = ["#ffbe0b", "#fb5607", "#ff006e", "#8338ec", "#3a86ff"];
+  const randomColor =
+    rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
   return randomColor;
 }
 
-function arrayClear(array) { // for getting RGB numbers
-  let newArray = array.filter((x => {
-    if ( x === " " || x === "(" || x === ")") {
-      return false
+function arrayClear(array) {
+  // for getting RGB numbers
+  let newArray = array.filter((x) => {
+    if (x === " " || x === "(" || x === ")") {
+      return false;
     }
     if (!Number(x) && Number(x) !== 0 && x !== ",") {
-      return false
+      return false;
     }
 
-    return true
-  }))
-  const A = []
-  const B = []
-  const C = []
+    return true;
+  });
+  const A = [];
+  const B = [];
+  const C = [];
   for (x of newArray) {
     if (x !== ",") {
-      A.push(x)
-    }
-    else {
+      A.push(x);
+    } else {
       for (let i = 0; i <= A.length; i++) {
-        newArray.shift()
+        newArray.shift();
       }
-      break
+      break;
     }
   }
   for (x of newArray) {
     if (x !== ",") {
-      B.push(x)
-    }
-    else {
+      B.push(x);
+    } else {
       for (let i = 0; i <= B.length; i++) {
-        newArray.shift()
+        newArray.shift();
       }
-      break
+      break;
     }
   }
   for (x of newArray) {
     if (x !== ",") {
-      C.push(x)
-    }
-    else {
-      break
+      C.push(x);
+    } else {
+      break;
     }
   }
-  console.log(A,B,C);
-  return [Number(A.join("")),Number(B.join("")),Number(C.join(""))]
+  console.log(A, B, C);
+  return [Number(A.join("")), Number(B.join("")), Number(C.join(""))];
 }
